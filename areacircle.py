f4cc2b19471df63184785263de9be46d644cbee9
rajdeepduarah <89378584+rajdeepduarah@users.noreply.github.com>
2021-08-23 13:07:27 +0530

    Add files via upload

---
+++ areacircle.py
@@ -0,0 +1,6 @@
+# This is a sample Python script.
+
+# Press Shift+F10 to execute it or replace it with your code.
+# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+
+print("area of the circle",3.14*float(input("enter the radius 0f circle="))**2)
